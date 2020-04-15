7f161491bd1cee2e331a2539c3c55c4d2d76ccbd
Manthan Desai <mkdesai@gmail.com>
2020-04-15 12:55:01 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v=^+q3$7h939=d-80@qr6nzxoi7!q_deuoyqmf5_ydsvfy=9e_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
